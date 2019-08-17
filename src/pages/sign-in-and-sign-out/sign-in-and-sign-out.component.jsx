16067039a1807e65313664f83dde402010ba239e
DominikRapacki <dr@picnature.de>
2019-08-17 20:39:58 +0200

    sign in page and form

---
+++ src/pages/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import SignIn from "../../components/sign-in/sign-in.component";
+
+import "./sign-in-and-sign-out.styles.scss"
+
+const SignInAndSignUpPage = () => {
+  return ( 
+    <div className="sign-in-and-sign-out"><SignIn /></div>
+
+   );
+}
+ 
+export default SignInAndSignUpPage;
