a21de661c705c2d9dc91e2ee35458247bcfcf939
DominikRapacki <dr@picnature.de>
2019-08-18 15:55:34 +0200

    added redux and implemented userReducer and userActions to header and app components

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React from "react";
 
 import "./custom-button.styles.scss";
 
-const CustomButton = ({children, isGoogleSignIn, ...otherProps}) => {
-  return ( 
-    <button className={`${isGoogleSignIn ? "google-sign-in" : ""} custom-button`} {...otherProps}>
+const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => {
+  return (
+    <button
+      className={`${isGoogleSignIn ? "google-sign-in" : ""} custom-button`}
+      {...otherProps}
+    >
       {children}
     </button>
+  );
+};
 
-   );
-}
- 
 export default CustomButton;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { Provider } from "react-redux";
+
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+
+import store from "./redux/store";
 // import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
