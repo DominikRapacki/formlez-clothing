e949dd31930b1a90fcb7b7cd3723cff00dfd2d9b
DominikRapacki <dr@picnature.de>
2019-08-27 12:55:33 +0200

    updated styled compoenents

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -15,12 +15,12 @@ const config = {
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
-  const userRef = firestore.doc(`users/${userAuth.uid}`)
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
   const snapShot = await userRef.get();
 
-  if(!snapShot.exists) {
-    const {displayName, email} = userAuth;
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
     const createdAt = new Date();
 
     try {
@@ -29,7 +29,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         email,
         createdAt,
         ...additionalData
-      })
+      });
     } catch (error) {
       console.log("error creating user", error.messagingSenderId);
     }
